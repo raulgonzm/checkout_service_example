72531446c3a9f755aab478a1265736a04644b93d
Raul Gonzalez Martinez <raul.gonzalez.martines@somosmuno.com>
2018-11-20 08:09:02 +0100

    product serializer added

---
+++ checkouts/apps/products/api/rest/v1_0/api_serializers.py
@@ -0,0 +1,11 @@
+# Python imports
+# Flask imports
+# Third-Party imports
+from marshmallow import Schema, fields
+# Project Imports
+
+
+class ProductSerializer(Schema):
+    code = fields.String(dump_only=True)
+    name = fields.String(dump_only=True)
+    price = fields.Decimal(dump_only=True, places=2)
---
+++ checkouts/apps/products/tests/api/rest/v1_0/test_api_serializers.py
@@ -0,0 +1,20 @@
+# Python imports
+import unittest
+# Flask imports
+# Third-Party imports
+# Project Imports
+from apps.products.api.rest.v1_0.api_serializers import ProductSerializer
+from apps.products.tests.mocks import ProductFactory
+
+
+class ProductSerializersTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.product = ProductFactory()
+
+    def test_product_serialization(self):
+        serializer_class = ProductSerializer()
+        result = serializer_class.dump(self.product)
+        self.assertEqual(result.data['code'], self.product.code)
+        self.assertEqual(result.data['name'], self.product.name)
+        self.assertEqual(result.data['price'], round(self.product.price, 2))
