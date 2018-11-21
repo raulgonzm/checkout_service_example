1d67bd4f4a46fdd571bcdde7ea1c4654bc37d775
Raul Gonzalez Martinez <raul.gonzalez.martines@somosmuno.com>
2018-11-21 20:53:32 +0100

    percentage discount skeleton

--- checkouts/apps/pricing_rules/percentage_discount.py
+++ checkouts/apps/pricing_rules/percentage_discount.py
@@ -7,14 +7,5 @@ from apps.pricing_rules.base_rules import PricingRule
 
 class PercentageDiscount(PricingRule):
 
-    def is_applicable_by_target_type(self, target):
-        pass
-
-    def is_applicable_by_quantity(self, quantity):
-        pass
-
-    def is_applicable_to(self, checkout):
-        pass
-
-    def apply_to_price_checkout(self, checkout):
+    def apply_to_price_purchase(self, purchase):
         pass
