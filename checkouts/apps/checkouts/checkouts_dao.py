33dfc030dfca89d0088e299e0c9cb0a8221dae57
Raul Gonzalez Martinez <raul.gonzalez.martines@somosmuno.com>
2018-11-22 17:56:57 +0100

    get checkout added to checkout DAO

--- checkouts/apps/checkouts/checkouts_dao.py
+++ checkouts/apps/checkouts/checkouts_dao.py
@@ -3,9 +3,14 @@
 # Third-Party imports
 # Project Imports
 from apps import db
+from apps.checkouts.models import Checkout
 
 
 def insert_checkout(checkout):
     db.session.add(checkout)
     db.session.commit()
     return checkout
+
+
+def get_checkout_by_id(checkout_id):
+    return Checkout.query.get(checkout_id)
--- checkouts/apps/checkouts/tests/test_checkouts_dao.py
+++ checkouts/apps/checkouts/tests/test_checkouts_dao.py
@@ -28,3 +28,10 @@ class CheckoutDAOTestCase(TestCase):
     def test_insert_product(self):
         new_checkout = checkouts_dao.insert_checkout(self.checkout)
         self.assertIsInstance(new_checkout.id, int)
+
+    def test_get_checkout_by_id(self):
+        new_checkout = CheckoutFactory()
+        new_checkout = checkouts_dao.insert_checkout(new_checkout)
+        self.assertIsInstance(new_checkout.id, int)
+        checkout_returned = checkouts_dao.get_checkout_by_id(new_checkout.id)
+        self.assertEqual(new_checkout, checkout_returned)
